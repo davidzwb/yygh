b2bc10691009e6cae8e0253fc5221625ca56f9a8
David Zhao <davidzwb@gmail.com>
2022-06-03 01:53:04 +0800

    Add data dictionary

---
+++ service/service_cmn/src/main/java/com/atguigu/yygh/cmn/service/impl/DictServiceImpl.java
@@ -0,0 +1,78 @@
+package com.atguigu.yygh.cmn.service.impl;
+
+import com.alibaba.excel.EasyExcel;
+import com.atguigu.yygh.cmn.listener.DictListener;
+import com.atguigu.yygh.cmn.mapper.DictMapper;
+import com.atguigu.yygh.cmn.service.DictService;
+import com.atguigu.yygh.model.cmn.Dict;
+import com.atguigu.yygh.vo.cmn.DictEeVo;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.BeanUtils;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class DictServiceImpl extends ServiceImpl<DictMapper, Dict> implements DictService {
+    @Override
+    @Cacheable(value = "dict", keyGenerator = "keyGenerator")
+    public List<Dict> findChildData(Long id) {
+        QueryWrapper<Dict> wrapper = new QueryWrapper<>();
+        wrapper.eq("parent_id", id);
+        List<Dict> list = baseMapper.selectList(wrapper);
+        list.stream().forEach(item -> item.setHasChildren(isChildren(item.getId())));
+        return list;
+    }
+
+    @Override
+    public void exportData(HttpServletResponse response) {
+
+        try {
+            response.setContentType("application/vnd.ms-excel");
+            response.setCharacterEncoding("utf-8");
+            // 这里URLEncoder.encode可以防止中文乱码 当然和easyexcel没有关系
+            String fileName = URLEncoder.encode("数据字典", "UTF-8");
+            response.setHeader("Content-disposition", "attachment;filename=" + fileName + ".xlsx");
+
+            List<Dict> dictList = baseMapper.selectList(null);
+            List<DictEeVo> dictVoList = new ArrayList<>(dictList.size());
+            for (Dict dict : dictList) {
+                DictEeVo dictVo = new DictEeVo();
+                BeanUtils.copyProperties(dict, dictVo);
+                dictVoList.add(dictVo);
+            }
+
+            EasyExcel.write(response.getOutputStream(), DictEeVo.class).sheet("数据字典").doWrite(dictVoList);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    @CacheEvict(value = "dict", allEntries = true)
+    public void importData(MultipartFile file) {
+        try {
+            EasyExcel.read(file.getInputStream(), DictEeVo.class, new DictListener(baseMapper))
+                    .sheet()
+                    .doRead();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean isChildren(Long id) {
+        QueryWrapper<Dict> wrapper = new QueryWrapper<>();
+        wrapper.eq("parent_id", id);
+        Integer count = baseMapper.selectCount(wrapper);
+        // 0>0    1>0
+        return count > 0;
+    }
+}
