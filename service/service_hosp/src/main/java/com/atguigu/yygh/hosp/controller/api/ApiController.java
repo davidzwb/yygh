93ad3afdfe8ae026c243ffd980da7fba5cc8c879
David Zhao <davidzwb@gmail.com>
2022-06-04 22:31:49 +0800

    Add third party hospital-manage

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/controller/api/ApiController.java
@@ -0,0 +1,4 @@
+package com.atguigu.yygh.hosp.controller.api;
+
+public class ApiController {
+}
---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/DepartmentService.java
@@ -0,0 +1,3 @@
+package com.atguigu.yygh.hosp.service.impl;
+
+public interface DepartmentService {}
