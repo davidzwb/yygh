c7d24f6b637edc68b0894d1fede17bc65b19763b
David Zhao <davidzwb@gmail.com>
2022-06-05 00:25:21 +0800

    Add nacos registration

--- service/service_cmn/src/main/java/com/atguigu/yygh/cmn/ServiceCmnApplication.java
+++ service/service_cmn/src/main/java/com/atguigu/yygh/cmn/ServiceCmnApplication.java
@@ -2,9 +2,11 @@ package com.atguigu.yygh.cmn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = "com.atguigu.yygh")
 public class ServiceCmnApplication {
     public static void main(String[] args) {
--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/ServiceHospApplication.java
@@ -2,9 +2,11 @@ package com.atguigu.yygh.hosp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @ComponentScan(basePackages = "com.atguigu.yygh")
 public class ServiceHospApplication {
     public static void main(String[] args) {
