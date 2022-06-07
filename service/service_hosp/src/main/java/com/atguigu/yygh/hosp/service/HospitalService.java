483607532053e1675f72f850973ce95d540cf8c7
David Zhao <davidzwb@gmail.com>
2022-06-08 04:42:32 +0800

    Add hospital management interfaces

--- service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/HospitalService.java
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/HospitalService.java
@@ -1,11 +1,20 @@
 package com.atguigu.yygh.hosp.service;
 
 import com.atguigu.yygh.model.hosp.Hospital;
+import com.atguigu.yygh.vo.hosp.HospitalQueryVo;
+import org.springframework.data.domain.Page;
 
+import java.util.List;
 import java.util.Map;
 
 public interface HospitalService {
-    void save(Map<String, Object> paramMap);
+  void save(Map<String, Object> paramMap);
 
-    Hospital getByHoscode(String hoscode);
+  Hospital getByHoscode(String hoscode);
+
+  Page<Hospital> selectPage(Integer page, Integer limit, HospitalQueryVo hospitalQueryVo);
+
+  void updateStatus(String id, Integer status);
+
+  Map<String, Object> show(String id);
 }
